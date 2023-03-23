7fdecd4d4c0073b45e1edbc277c5647ed48e69b4
Agnieszka <agnieszkagrzymska1412@gmail.com>
2023-03-23 15:13:17 +0100

    Main project files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# aquarium
- 3D model of an aquarium with animated elements
+## Aquarium
+3D model of an aquarium with animated fish.
+
+Project was written in C++ using OpenGL.
+To run the project following libraries must be installed:
+- GLFW (https://www.glfw.org/download.html)
+- assimp (https://github.com/assimp/assimp/blob/master/Build.md)
+
+Created by:
+Agnieszka Grzymska and Łukasz Jankowski
+
+
---
+++ project/include/ObjLoader.h
@@ -0,0 +1,140 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<sstream>
+#include<algorithm>
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Vertex.h"
+
+static std::vector<Vertex> loadOBJ(const char* file_name) {
+
+//	std::cout << "hello";
+
+	std::vector<glm::fvec3> vertex_positions;
+	std::vector<glm::fvec2> vertex_texcoords;
+	std::vector<glm::fvec3> vertex_normals;
+
+	std::vector<GLint> vertex_position_indicies;
+	std::vector<GLint> vertex_texcoord_indicies;
+	std::vector<GLint> vertex_normal_indicies;
+
+
+	std::vector<Vertex> vertices;
+
+	std::stringstream ss;
+	std::ifstream in_file(file_name);
+	std::string line = "";
+	std::string prefix = "";
+
+	glm::vec3 temp_vec3;
+	glm::vec2 temp_vec2;
+	GLint temp_glint;
+	
+	if (!in_file.is_open()) {
+		//throw "ERROR::OBJLOADER::Could not open file";
+		//return;
+
+	}
+	
+	while (std::getline(in_file, line))
+	{
+		ss.clear();
+		ss.str(line);
+		ss >> prefix;
+
+		if (prefix == "#") {
+
+		}
+
+		else if (prefix == "o") {
+
+		}
+		else if (prefix == "use_mtl") {
+
+		}
+
+		else if (prefix == "v") {
+			// vertex position
+			ss >> temp_vec3.x >> temp_vec3.y >> temp_vec3.z;
+			vertex_positions.push_back(temp_vec3);
+		}
+
+		else if (prefix == "vt") {
+			ss >> temp_vec2.x >> temp_vec2.y;
+			vertex_texcoords.push_back(temp_vec3);
+		}
+
+		else if (prefix == "vn") {
+			ss >> temp_vec3.x >> temp_vec3.y >> temp_vec3.z;
+			vertex_normals.push_back(temp_vec3);
+		}
+		else if (prefix == "f") {
+
+			int counter = 0;
+			while (ss >> temp_glint)
+			{
+				//Pushing indices into correct arrays
+				if (counter == 0)
+					vertex_position_indicies.push_back(temp_glint);
+				else if (counter == 1)
+					vertex_texcoord_indicies.push_back(temp_glint);
+				else if (counter == 2)
+					vertex_normal_indicies.push_back(temp_glint);
+
+				//Handling characters
+				if (ss.peek() == '/')
+				{
+					++counter;
+					ss.ignore(1, '/');
+				}
+				else if (ss.peek() == ' ')
+				{
+					++counter;
+					ss.ignore(1, ' ');
+				}
+
+				//Reset the counter
+				if (counter > 2)
+					counter = 0;
+			}
+
+
+		}
+
+		else {
+
+		}
+
+		vertices.resize(vertex_position_indicies.size(), Vertex());
+
+		//Load in all indices
+		for (size_t i = 0; i < vertices.size(); ++i)
+		{
+			vertices[i].position = vertex_positions[vertex_position_indicies[i] - 1];
+			vertices[i].texcoord = vertex_texcoords[vertex_texcoord_indicies[i] - 1];
+			vertices[i].normal = vertex_normals[vertex_normal_indicies[i] - 1];
+			vertices[i].color = glm::vec3(1.f, 1.f, 1.f);
+		}
+
+
+	//	std::cout << line << "\n";
+	//	std::cout << "Nr of verties: " << vertex_positions.size() << "\n";
+
+
+	}
+
+	return vertices;
+
+
+}
